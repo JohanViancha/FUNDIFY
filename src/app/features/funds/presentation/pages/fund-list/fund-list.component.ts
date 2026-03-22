e1a09d02070c350b85de99ebfbb5a4eeaa4b6a40
Johan Viancha <vianchajohan@gmail.com>
2026-03-22 16:13:34 -0500

    implementar caso de uso para mostrar la lista de fondos

--- src/app/features/funds/presentation/pages/fund-list/fund-list.component.ts
+++ src/app/features/funds/presentation/pages/fund-list/fund-list.component.ts
@@ -1,17 +1,11 @@
-import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-
-interface Fund {
-  id: string;
-  name: string;
-  minAmount: number;
-  category: string;
-}
-
+import { MatTableModule } from '@angular/material/table';
+import { Fund } from '../../../domain/models/fund.model';
+import { GetFundsUseCase } from '../../../application/use-cases/get-funds.use-case';
 @Component({
   selector: 'app-fund-list',
   standalone: true,
@@ -19,10 +13,10 @@ interface Fund {
   templateUrl: './fund-list.component.html',
 })
 export class FundListComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'name', 'category', 'minAmount', 'actions'];
+  private getFundsUseCase = inject(GetFundsUseCase);
 
+  displayedColumns: string[] = ['id', 'name', 'category', 'minimumAmount', 'actions'];
   funds = signal<Fund[]>([]);
-  userBalance = signal<number>(100000);
 
   constructor(private snackBar: MatSnackBar) {}
 
@@ -31,47 +25,7 @@ export class FundListComponent implements OnInit {
   }
 
   loadFunds() {
-    this.funds.set([
-      {
-        id: '1',
-        name: 'Fondo de Pensiones Voluntarias',
-        minAmount: 75000,
-        category: 'FPV_BTG_PACTUAL_RECAUDADORA',
-      },
-      {
-        id: '2',
-        name: 'Fondo de Pensiones Voluntarias',
-        minAmount: 125000,
-        category: 'FPV_BTG_PACTUAL_ECOPETROL',
-      },
-      { id: '3', name: 'Fondo de Inversión Colectiva', minAmount: 50000, category: 'DEUDAPRIVADA' },
-      {
-        id: '4',
-        name: 'Fondo de Inversión Colectiva',
-        minAmount: 250000,
-        category: 'FDO-ACCIONES',
-      },
-      {
-        id: '5',
-        name: 'Fondo de Pensiones Voluntarias',
-        minAmount: 100000,
-        category: 'FPV_BTG_PACTUAL_DINAMICA',
-      },
-    ]);
+    this.getFundsUseCase.execute().subscribe((funds) => this.funds.set(funds));
   }
 
-  subscribe(fund: Fund) {
-    if (this.userBalance() < fund.minAmount) {
-      this.snackBar.open('No tienes saldo suficiente', 'Cerrar', {
-        duration: 3000,
-      });
-      return;
-    }
-
-    this.userBalance.update((balance) => balance - fund.minAmount);
-
-    this.snackBar.open(`Te suscribiste a ${fund.name}`, 'OK', {
-      duration: 3000,
-    });
-  }
 }
