c69356f6b94d26083d08fbcecc3484d7f776772c
Johan Viancha <vianchajohan@gmail.com>
2026-03-22 14:16:38 -0500

    ajustar appconfig para proveeder el repository de fondos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { FUND_REPOSITORY_TOKEN } from './features/funds/domain/repositories/fund.tokens';
+import { FundJsonService } from './features/funds/infrastructure/api/fund-json.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: FUND_REPOSITORY_TOKEN, useClass: FundJsonService }
   ]
 };
