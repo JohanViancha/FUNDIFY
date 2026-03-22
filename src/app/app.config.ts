96e6a0796e84ba748a9cc9bf4a4951ba6eb14d86
Johan Viancha <vianchajohan@gmail.com>
2026-03-22 15:30:01 -0500

    agregar provider en la configuracion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import localeEsCo from '@angular/common/locales/es-CO';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { registerLocaleData } from '@angular/common';
 import { routes } from './app.routes';
 import { FUND_REPOSITORY_TOKEN } from './features/funds/domain/repositories/fund.tokens';
 import { FundJsonService } from './features/funds/infrastructure/api/fund-json.service';
+import { TRANSACTION_REPOSITORY_TOKEN } from './features/transactions/domain/repositories/transaction.tokens';
+import { TransactionJsonService } from './features/transactions/infrastructure/api/transaction-json.service';
+registerLocaleData(localeEsCo);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    { provide: FUND_REPOSITORY_TOKEN, useClass: FundJsonService }
-  ]
+    { provide: LOCALE_ID, useValue: 'es-CO' },
+    { provide: FUND_REPOSITORY_TOKEN, useClass: FundJsonService },
+    { provide: TRANSACTION_REPOSITORY_TOKEN, useClass: TransactionJsonService },
+  ],
 };
