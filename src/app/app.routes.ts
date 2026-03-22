1b9b73aa4c6c43fb8f172aa4bc750b9e826959fe
Johan Viancha <vianchajohan@gmail.com>
2026-03-22 15:28:50 -0500

    definir rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,23 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component/layout.component';
 
 export const routes: Routes = [
   {
-    path: 'funds',
-    loadChildren: () =>
-      import('./features/funds/presentation/funds.routes').then((m) => m.FUNDS_ROUTES),
-  },
-  {
-    path: 'transactions',
-    loadChildren: () =>
-      import('./features/transactions/presentation/transactions.routes').then((m) => m.TRANSACTIONS_ROUTES),
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'funds',
+        loadChildren: () =>
+          import('./features/funds/presentation/funds.routes').then((m) => m.FUNDS_ROUTES),
+      },
+      {
+        path: 'transactions',
+        loadChildren: () =>
+          import('./features/transactions/presentation/transactions.routes').then(
+            (m) => m.TRANSACTIONS_ROUTES,
+          ),
+      }
+    ]
   },
 ];
