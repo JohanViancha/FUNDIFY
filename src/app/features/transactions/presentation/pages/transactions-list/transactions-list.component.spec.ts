5f378d022ec979e99a47d75bebfcb2f777355fe1
Johan Viancha <vianchajohan@gmail.com>
2026-03-24 01:09:26 -0500

    crear componente para listar las transacciones

---
+++ src/app/features/transactions/presentation/pages/transactions-list/transactions-list.component.spec.ts
@@ -0,0 +1,125 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest';
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { registerLocaleData } from '@angular/common';
+import localeEsCo from '@angular/common/locales/es-CO';
+
+import { TransactionsListComponent } from './transactions-list.component';
+import { GetTransactionsUseCase } from '../../../application/use-cases/get-transactions.use-case';
+import { TransactionDto } from '../../../application/dtos/transaction.dto';
+registerLocaleData(localeEsCo);
+
+describe('TransactionsListComponent', () => {
+  let component: TransactionsListComponent;
+  let fixture: ComponentFixture<TransactionsListComponent>;
+
+  const mockTransactions: TransactionDto[] = [
+    {
+      id: 't1',
+      fundName: 'Fondo Pensiones',
+      type: 'SUBSCRIPTION',
+      amount: 100000,
+      date: new Date('2024-01-01'),
+    },
+    {
+      id: 't2',
+      fundName: 'Fondo Inversión',
+      type: 'CANCELLATION',
+      amount: 50000,
+      date: new Date('2024-02-01'),
+    },
+  ];
+
+  const getTransactionsUseCaseMock = {
+    execute: vi.fn(),
+  };
+
+  beforeEach(async () => {
+    getTransactionsUseCaseMock.execute.mockReturnValue(of(mockTransactions));
+
+    await TestBed.configureTestingModule({
+      imports: [TransactionsListComponent],
+      providers: [
+        {
+          provide: GetTransactionsUseCase,
+          useValue: getTransactionsUseCaseMock,
+        },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TransactionsListComponent);
+
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call execute on init', () => {
+    expect(getTransactionsUseCaseMock.execute).toHaveBeenCalled();
+  });
+
+  it('should load transactions into signal', () => {
+    const transactions = component.transactions();
+
+    expect(transactions.length).toBe(2);
+  });
+
+  it('should render transaction rows', () => {
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    const rows = compiled.querySelectorAll('tr');
+
+    expect(rows.length).toBeGreaterThan(1);
+
+    expect(compiled.textContent).toContain('Fondo Pensiones');
+
+    expect(compiled.textContent).toContain('Fondo Inversión');
+  });
+
+  it('should show empty state when no transactions', async () => {
+    getTransactionsUseCaseMock.execute.mockReturnValue(of([]));
+
+    fixture = TestBed.createComponent(TransactionsListComponent);
+
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.textContent).toContain('No hay transacciones registradas.');
+  });
+
+  it('should return correct label for SUBSCRIPTION', () => {
+    const label = component.getTypeLabel('SUBSCRIPTION');
+
+    expect(label).toBe('Suscripción');
+  });
+
+  it('should return correct label for CANCELLATION', () => {
+    const label = component.getTypeLabel('CANCELLATION');
+
+    expect(label).toBe('Cancelación');
+  });
+
+  it('should return primary-chip for SUBSCRIPTION', () => {
+    const color = component.getColor('SUBSCRIPTION');
+
+    expect(color).toBe('primary-chip');
+  });
+
+  it('should return error-chip for CANCELLATION', () => {
+    const color = component.getColor('CANCELLATION');
+
+    expect(color).toBe('error-chip');
+  });
+});
--- src/app/features/transactions/presentation/pages/transactions-list/transactions-list.component.ts
+++ src/app/features/transactions/presentation/pages/transactions-list/transactions-list.component.ts
@@ -1,15 +1,42 @@
 import { CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatChipsModule } from '@angular/material/chips';
 import { GetTransactionsUseCase } from '../../../application/use-cases/get-transactions.use-case';
 import { TransactionDto } from '../../../application/dtos/transaction.dto';
-import { TypeTransaction } from '../../../domain/models/transaction.model';
+import { TransactionType } from '../../../domain/models/transaction.model';
+import { PaginationComponent } from '../../components/pagination/pagination.component';
+import { PageEvent } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-transactions-list',
-  imports: [CommonModule, MatTableModule, MatCardModule, MatChipsModule, DatePipe, CurrencyPipe],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatCardModule,
+    MatChipsModule,
+    DatePipe,
+    CurrencyPipe,
+    PaginationComponent,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './transactions-list.componet.html',
   styleUrl: './transactions-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,6 +44,10 @@ import { TypeTransaction } from '../../../domain/models/transaction.model';
 export class TransactionsListComponent implements OnInit {
   private getTransactionsUseCase = inject(GetTransactionsUseCase);
   displayedColumns: string[] = ['fundName', 'type', 'amount', 'date'];
+  public readonly filterType = signal<TransactionType | 'ALL'>('ALL');
+  public readonly pageSize = signal(10);
+  public readonly pageIndex = signal(0);
+  public readonly filterFund = signal('');
 
   transactions = signal<TransactionDto[]>([]);
 
@@ -25,14 +56,58 @@ export class TransactionsListComponent implements OnInit {
   }
 
   loadTransactions() {
-    this.getTransactionsUseCase.execute().subscribe((transactions) => this.transactions.set(transactions));
+    this.getTransactionsUseCase
+      .execute()
+      .subscribe((transactions) => this.transactions.set(transactions));
   }
 
-  getTypeLabel(type: TypeTransaction): string {
+  getTypeLabel(type: TransactionType): string {
     return type === 'SUBSCRIPTION' ? 'Suscripción' : 'Cancelación';
   }
 
-  getColor(type: TypeTransaction): string {
+  getColor(type: TransactionType): string {
     return type === 'SUBSCRIPTION' ? 'primary-chip' : 'error-chip';
+  }
+
+  public readonly typeOptions: Array<{ value: TransactionType | 'ALL'; label: string }> = [
+    { value: 'ALL', label: 'Todas' },
+    { value: 'SUBSCRIPTION', label: 'Suscripciones' },
+    { value: 'CANCELLATION', label: 'Cancelaciones' },
+  ];
+
+  public readonly filteredTransactions = computed(() => {
+    let filtered = this.transactions();
+
+    const fundQuery = this.filterFund().toLowerCase().trim();
+    if (fundQuery) {
+      filtered = filtered.filter((tx) => tx.fundName.toLowerCase().includes(fundQuery));
+    }
+
+    if (this.filterType() !== 'ALL') {
+      filtered = filtered.filter((tx) => tx.type === this.filterType());
+    }
+    return filtered;
+  });
+
+  public readonly paginationConfig = computed(() => ({
+    totalCount: this.filteredTransactions().length,
+    pageSize: this.pageSize(),
+    pageIndex: this.pageIndex(),
+  }));
+
+  public readonly displayedTransactions = computed(() => {
+    const filtered = this.filteredTransactions();
+    const start = this.pageIndex() * this.pageSize();
+    return filtered.slice(start, start + this.pageSize());
+  });
+
+  onPageChange(event: PageEvent) {
+    this.pageIndex.set(event.pageIndex);
+    this.pageSize.set(event.pageSize);
+  }
+
+  onFilterChange(type: TransactionType | 'ALL') {
+    this.filterType.set(type);
+    this.pageIndex.set(0);
   }
 }
