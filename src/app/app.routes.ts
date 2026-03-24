3b95c46c4a02c42c5ca3c572dc93445206c9b2fd
Johan Viancha <vianchajohan@gmail.com>
2026-03-23 23:51:33 -0500

    ajustar rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,11 @@ export const routes: Routes = [
     component: LayoutComponent,
     children: [
       {
+        path: "",
+        redirectTo: "funds",
+        pathMatch: "full"
+      },
+      {
         path: 'funds',
         loadChildren: () =>
           import('./features/funds/presentation/funds.routes').then((m) => m.FUNDS_ROUTES),
@@ -16,6 +21,13 @@ export const routes: Routes = [
         loadChildren: () =>
           import('./features/transactions/presentation/transactions.routes').then(
             (m) => m.TRANSACTIONS_ROUTES,
+          ),
+      },
+      {
+        path: 'portfolio',
+        loadChildren: () =>
+          import('./features/portfolio/presentation/portfolio.routes').then(
+            (m) => m.PORTFOLIO_ROUTES,
           ),
       }
     ]
